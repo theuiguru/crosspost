d8eb87c7c215d4c12944d169498be9176c927a2a
Chris Thomas <webmaster102@gmail.com>
2024-12-05 14:00:42 -0500

    added index.js

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # crosspost
-A JavaScript utility for posting across multiple social networks at once.
+A JavaScript utility for posting across multiple social networks at once based on Nicholas C. Zakas' (@humanwhocodes) [Github Repo](https://github.com/humanwhocodes/crosspost) and [npm package](https://www.npmjs.com/package/@humanwhocodes/crosspost)
---
+++ index.js
@@ -0,0 +1,28 @@
+import {
+	Client,
+	TwitterStrategy,
+	BlueskyStrategy,
+} from "@humanwhocodes/crosspost";
+
+// Note: Use an app password, not your login password!
+const bluesky = new BlueskyStrategy({
+	identifier: "cthomas.bsky.social",
+	password: process.env.BSKY_APP_PASSWORD,
+	host: "bsky.social", // "bsky.social" for most people
+});
+
+// Note: OAuth app is required
+const twitter = new TwitterStrategy({
+	accessTokenKey: process.env.TX_ACCESS_TOKEN,
+	accessTokenSecret: process.env.TX_ACCESS_TOKEN_SECRET,
+	apiConsumerKey: process.env.TX_API_CONSUMER_KEY,
+	apiConsumerSecret: process.env.TX_CONSUMER_SECRET,
+});
+
+// create a client that will post to all three
+const client = new Client({
+	strategies: [bluesky, twitter],
+});
+
+// post to all two
+await client.post("Success in crossposting to X and Bluesky!");
